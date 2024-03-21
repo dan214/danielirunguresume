18f500ff113b3e8e31c15d09e43a400cc4973fe4
Daniel Irungu <irungu214@gmail.com>
2024-03-21 12:41:50 +0300

    add contact me page and emailJS service

--- src/app/contactme/page.tsx
+++ src/app/contactme/page.tsx
@@ -1,7 +1,102 @@
+"use client"
+import React, { useRef, useState } from 'react';
+import { Button, Form, FormGroup, Label, Input, Container } from 'reactstrap';
+import emailjs from 'emailjs-com';
+
 export default function Page() {
+
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        message: '',
+    });
+
+    const [messageSent, setMessageSent] = useState(false);
+
+    const form = useRef();
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+        const { name, value } = e.target;
+        setFormData(prevState => ({
+            ...prevState,
+            [name]: value,
+        }));
+    };
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        console.log("e")
+        console.log(e)
+        console.log(form.current)
+
+        emailjs
+            .sendForm('service_04i5wbi', 'template_niqf7sp', e.target, 'jltWkwjye-Du-lq6D')
+            .then(
+                () => {
+                    console.log('SUCCESS!');
+                    setMessageSent(true);
+                },
+                (error) => {
+                    console.log('FAILED...', error.text);
+                },
+            );
+
+        setFormData({
+            name: '',
+            email: '',
+            message: '',
+        });
+    };
+
     return (
         <>
-            <div className="header-page"><h2>Contact Us</h2></div>
+            <div className="header-page">
+                <h2>Contact Me</h2>
+            </div>
+            <Container>
+                <h2>Contact Me</h2>
+                {!messageSent ? (
+                    <Form onSubmit={sendEmail}>
+                        <FormGroup>
+                            <Label for="name">Name</Label>
+                            <Input
+                                type="text"
+                                name="name"
+                                id="name"
+                                value={formData.name}
+                                onChange={handleChange}
+                                required
+                            />
+                        </FormGroup>
+                        <FormGroup>
+                            <Label for="email">Email</Label>
+                            <Input
+                                type="email"
+                                name="email"
+                                id="email"
+                                value={formData.email}
+                                onChange={handleChange}
+                                required
+                            />
+                        </FormGroup>
+                        <FormGroup>
+                            <Label for="message">Message</Label>
+                            <Input
+                                type="textarea"
+                                name="message"
+                                id="message"
+                                value={formData.message}
+                                onChange={handleChange}
+                                required
+                            />
+                        </FormGroup>
+                        <Button type="submit" color="primary">Send</Button>
+                    </Form>
+                ) : (
+                    <p>Email has been sent. Thank you!</p> // Display message if email sent
+                )}
+            </Container>
         </>
     )
 }
